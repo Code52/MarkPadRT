43caa43ca448411cb197302418ec55862acbec27
Paul <paul@jenkinsfamily.com.au>
2012-09-03 15:51:39 +1000

    init

---
+++ src/MarkPadLite/App.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace MarkPadLite
+{
+    sealed partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
+            Suspending += OnSuspending;
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                if (args.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Load state from previously suspended application
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null)
+            {
+                // When the navigation stack isn't restored navigate to the first page,
+                // configuring the new page by passing required information as a navigation
+                // parameter
+                if (!rootFrame.Navigate(typeof(MainPage), args.Arguments))
+                {
+                    throw new Exception("Failed to create initial page");
+                }
+            }
+            // Ensure the current window is active
+            Window.Current.Activate();
+        }
+
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+    }
+}
---
+++ src/MarkPadLite/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Windows.UI.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace MarkPadLite
+{
+    public sealed partial class MainPage
+    {
+        private DispatcherTimer t = new DispatcherTimer();
+        MarkdownDeep.Markdown x = new MarkdownDeep.Markdown();
+        public MainPage()
+        {
+            InitializeComponent();
+            t.Interval = new TimeSpan(0,0,0,0,500);
+            t.Tick += t_Tick;
+            wv.LoadCompleted += wv_LoadCompleted;
+        }
+
+        void wv_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+            tx.Focus(FocusState.Keyboard);
+        }
+
+        void t_Tick(object sender, object e)
+        {
+            t.Stop();
+            string foo;
+            tx.Document.GetText(TextGetOptions.None, out foo);
+            wv.NavigateToString(x.Transform(foo));
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private void tx_TextChanged_2(object sender, RoutedEventArgs e)
+        {
+            t.Stop();
+            t.Start();
+        }
+    }
+}
