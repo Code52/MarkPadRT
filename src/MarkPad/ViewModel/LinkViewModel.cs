9ec02d5597367c3ccca623a0de25b23934e27dd5
Ian Randall <ian@xaml.geek.nz>
2012-09-16 23:15:35 +1200

    hyperlink implementation

---
+++ src/MarkPad/ViewModel/LinkViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarkPad.Common;
+
+namespace MarkPad.ViewModel
+{
+    public  class LinkViewModel : BindableBase
+    {
+        private string _displayText;
+        private string _linkAddress;
+
+        public string DisplayText
+        {
+            get { return _displayText; }
+            set
+            {
+                if (value == _displayText) return;
+                _displayText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string LinkAddress
+        {
+            get { return _linkAddress; }
+            set
+            {
+                if (value == _linkAddress) return;
+                _linkAddress = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
--- src/MarkPad/Views/LinkDialog.xaml.cs
+++ src/MarkPad/Views/LinkDialog.xaml.cs
@@ -1,14 +1,29 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using MarkPad.ViewModel;
+using Windows.UI.Xaml;
 
 namespace MarkPad.Views
 {
     public sealed partial class LinkDialog : IDialog
     {
-        public LinkDialog()
+        public LinkDialog(string selectedText)
         {
             InitializeComponent();
+
+            //  Check if the selected text already is a link...
+            string text = selectedText, url = string.Empty;
+            var match = Regex.Match(selectedText, @"\[(?<text>(?:[^\\]|\\.)+)\]\((?<url>[^)]+)\)");
+            if (match.Success)
+            {
+                text = match.Groups["text"].Value;
+                url = match.Groups["url"].Value;
+            }
+
+            DataContext = new LinkViewModel {DisplayText = text, LinkAddress = url};
         }
 
+        public Action Added { get; set; }
         public Action Cancelled { get; set; }
 
         private void BackgroundGridClicked(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
@@ -20,6 +35,20 @@ namespace MarkPad.Views
         private void ForegroundGridClicked(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
         {
             e.Handled = true;
+        }
+
+        private void OnAdd(object sender, RoutedEventArgs e)
+        {
+            if (Added != null)
+                Added();
+
+            if (Cancelled != null) Cancelled();
+        }
+
+        private void OnCancel(object sender, RoutedEventArgs e)
+        {
+            if (Cancelled != null)
+                Cancelled();
         }
     }
 }
