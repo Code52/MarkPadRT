75ddfade71a6a9489d85cacfa46d9c4a8a8fea8a
Ian Randall <ian@xaml.geek.nz>
2012-09-16 22:05:20 +1200

    merge

---
+++ src/MarkPad/Views/LinkDialog.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MarkPad.Views
+{
+    public sealed partial class LinkDialog : IDialog
+    {
+        public LinkDialog()
+        {
+            InitializeComponent();
+        }
+
+        public Action Cancelled { get; set; }
+
+        private void BackgroundGridClicked(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            if (Cancelled != null)
+                Cancelled();
+        }
+
+        private void ForegroundGridClicked(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+}
